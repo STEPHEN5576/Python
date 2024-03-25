3adbaa4f3a1ae7d017e9eef2f53aac1821cbcaca
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-25 15:04:36 +0530

    commited

--- RandomNumberguess/app.py
+++ RandomNumberguess/app.py
@@ -12,9 +12,17 @@ else:
     quit()
 
 random_number = random.randint(0,top_of_range)
-if(top_of_range == random_number):
-    print('Thats correct')
-else:
-    print('its wrong answer please try again') 
-    print('the number was '+ str(random_number))   
+  
+while True:
+    user_guess = input('Make a guess: ')
+    if(user_guess.isdigit):
+        user_guess = int(user_guess)
+    else:
+        print('please enter digit next time.')
+        continue
+    if user_guess == random_number:
+        print('You got it')
+        break
+    else:
+        print('You got it worng')
 
